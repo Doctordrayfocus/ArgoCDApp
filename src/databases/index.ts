4b9040a7ba4937469acb30b6d33efa00de092c1e
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-06-11 02:23:19 +0100

    initial commit

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,10 +1,14 @@
 import { join } from 'path';
 import { ConnectionOptions } from 'typeorm';
-import { root } from './path';
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from '@config';
 
 export const dbConnection: ConnectionOptions = {
-  type: 'sqlite',
-  database: `${root}/data/argo.sqlite`,
+  type: 'postgres',
+  host: DB_HOST,
+  port: parseInt(DB_PORT),
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   synchronize: true,
   logging: false,
   entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
