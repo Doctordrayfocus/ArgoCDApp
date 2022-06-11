533ebcc7582ad94c53f14d36e5de3be635d5d328
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-06-11 02:21:23 +0100

    initial commit

---
+++ src/databases/index.ts
@@ -0,0 +1,18 @@
+import { join } from 'path';
+import { ConnectionOptions } from 'typeorm';
+import { root } from './path';
+
+export const dbConnection: ConnectionOptions = {
+  type: 'sqlite',
+  database: `${root}/data/argo.sqlite`,
+  synchronize: true,
+  logging: false,
+  entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '../**/*.migration{.ts,.js}')],
+  subscribers: [join(__dirname, '../**/*.subscriber{.ts,.js}')],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
